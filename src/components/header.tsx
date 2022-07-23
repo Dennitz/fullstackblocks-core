def49c5f3f6291504d021a5e39d7b1c722b4b427
Dennis Hellweg <mail@dennishellweg.com>
2022-07-23 18:52:19 +0100

    Make header responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,23 +3,23 @@ import Link from "next/link";
 import { classNames } from "@/utils/class-names";
 
 export const Header: React.FC<{ authenticated?: boolean }> = (props = { authenticated: false }) => (
-  <nav className="mx-auto flex max-w-7xl py-6 px-10">
+  <nav className="mx-auto flex max-w-7xl py-6 px-10 flex-col md:flex-row space-y-5 md:space-y-0">
     <HeaderLink href="/">Home</HeaderLink>
-    <HeaderLink href="/client-side-protected" className="ml-10">
+    <HeaderLink href="/client-side-protected" className="md:ml-10">
       Protected (client-side)
     </HeaderLink>
-    <HeaderLink href="/server-side-protected" className="ml-10">
+    <HeaderLink href="/server-side-protected" className="md:ml-10">
       Protected (server-side)
     </HeaderLink>
-    <HeaderLink href="/app/middleware-protected" className="ml-10">
+    <HeaderLink href="/app/middleware-protected" className="md:ml-10">
       Protected (middleware)
     </HeaderLink>
     {props.authenticated ? (
-      <HeaderLink href="/api/auth/signout" className="ml-auto">
+      <HeaderLink href="/api/auth/signout" className="md:ml-auto">
         Sign out
       </HeaderLink>
     ) : (
-      <HeaderLink href="/api/auth/signin" className="ml-auto">
+      <HeaderLink href="/api/auth/signin" className="md:ml-auto">
         Sign in
       </HeaderLink>
     )}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { FunctionComponent } from "react";
 
 const Document: FunctionComponent<DocumentProps> = () => (
   <Html className="h-full bg-gray-50 antialiased" lang="en">
-    <Head />
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
     <body className="h-full">
       <Main />
       <NextScript />
