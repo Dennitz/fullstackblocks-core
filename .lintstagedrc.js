16a881bee8ce4a4ba71ffc53e500c154a7f431e5
Dennis Hellweg <mail@dennishellweg.com>
2022-08-08 19:11:26 +0200

    Add eslint/prettier commit hooks

---
+++ .lintstagedrc.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`;
+
+module.exports = {
+  "*.{ts,tsx,js,jsx}": [buildEslintCommand],
+  "*.{ts,tsx,js,jsx,md,yaml,yml}": "prettier --write",
+};
